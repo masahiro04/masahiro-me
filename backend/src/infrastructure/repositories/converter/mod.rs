955450ca38e2c6b804d2527d9b4f079641d573e7
masahirookubo <stmhamachiii@gmail.com>
2023-06-01 19:09:04 +0900

    refactor: deleted backend code

--- backend/src/infrastructure/repositories/converter/mod.rs
+++
@@ -1,2 +0,0 @@
-mod html_to_text_converter;
-pub mod posts_from_api_to_posts_converter;
--- backend/src/infrastructure/repositories/types/media_from_api.rs
+++
@@ -1,7 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
-pub struct MediaFromApi {
-    pub source_url: String,
-    pub alt_text: String,
-}
