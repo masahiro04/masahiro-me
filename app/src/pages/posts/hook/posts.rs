17f5d205b57e3ce7c8016942f11cd99e1b3462eb
masahirookubo <stmhamachiii@gmail.com>
2023-06-03 19:48:55 +0900

    fix: pagination error

--- app/src/pages/posts/hook/posts.rs
+++ app/src/pages/posts/hook/posts.rs
@@ -10,7 +10,11 @@ use yew::{
 
 const PER_PAGE: i32 = 10;
 
+pub struct OffsetState {
+    value: i32,
+}
 pub struct PostsState {
+    offset: OffsetState,
     susp: Suspension,
     value: Rc<RefCell<Option<Vec<Post>>>>,
 }
@@ -22,30 +26,34 @@ impl PostsState {
 
         {
             let value = value.clone();
-            // we use tokio spawn local here.
             spawn_local(async move {
-                // let user = fetch_user().await;
                 {
                     let mut value = value.borrow_mut();
-
                     let posts = match fetch_posts_usecase(PER_PAGE, offset).await {
                         Ok(posts) => posts,
                         Err(_) => vec![],
                     };
                     *value = Some(posts);
                 }
-
                 handle.resume();
             });
         }
 
-        Self { susp, value }
+        Self {
+            susp,
+            value,
+            offset: OffsetState { value: offset },
+        }
     }
 }
 
 #[hook]
 pub fn use_posts(offset: i32) -> SuspensionResult<Vec<Post>> {
     let posts_state = use_state(|| PostsState::new(offset));
+    if posts_state.offset.value != offset {
+        let new_state = PostsState::new(offset);
+        posts_state.set(new_state);
+    }
 
     let result = match *posts_state.value.borrow() {
         Some(ref user) => Ok(user.clone()),
