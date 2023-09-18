58784d0341f8f97cb5241250ec905ff0238c387c
masahirookubo <stmhamachiii@gmail.com>
2023-09-19 01:46:19 +0900

    test: post_repository.find_all

--- crates/infrastructure/src/infrastructure/repositories/post_repository.rs
+++ crates/infrastructure/src/infrastructure/repositories/post_repository.rs
@@ -22,14 +22,11 @@ impl IPostRepository for PostRepository {
             "{}/posts?per_page={}&offset={}",
             &self.api_url, per_page, offset
         );
-        println!("{}", url);
         let response = reqwest::Client::new()
             .get(url)
             .send()
             .await
             .map_err(|e| anyhow::anyhow!(e.to_string()))?;
-
-        println!("Status: {}", response.status());
         let posts_from_api = response
             .json::<Vec<PostFromApi>>()
             .await
@@ -83,12 +80,10 @@ impl IPostRepository for PostRepository {
 
 #[cfg(test)]
 mod tests {
-    use crate::repositories::post_repository::post_from_api::PostFromApi;
-
     use super::PostRepository;
+    use crate::repositories::post_repository::post_from_api::PostFromApi;
     use domain::entities::post::Post;
     use domain::repositories::post_repository::IPostRepository;
-    // use mockito::{Mock, ServerGuard};
 
     #[tokio::test]
     async fn test_find_all() -> anyhow::Result<()> {
@@ -102,71 +97,34 @@ mod tests {
             tags: vec![],
             featured_media: "featured_media".to_string(),
         }];
-
         let posts = posts_from_api
             .clone()
             .into_iter()
             .map(|post_from_api| post_from_api.into_post().unwrap())
             .collect::<Vec<Post>>();
-
         let json_string = serde_json::to_string(&posts_from_api).unwrap();
-        let mut server = mockito::Server::new();
-        println!("sentinel1");
+        let mock = create_mock_server("/posts?per_page=1&offset=1".to_string(), json_string, None);
+        let repository = PostRepository::new(mock.0.url());
 
-        // Use one of these addresses to configure your client
-        let host = server.host_with_port();
-        let url = server.url();
-
-        println!("{}", host);
-        println!("{}", url);
-        println!("{}", json_string);
-        println!("{}", server.host_with_port());
-        // Create a mock
-
-        let mock = server
-            .mock("GET", "/posts?per_page=1&offset=1")
-            .with_status(200)
-            .with_header("content-type", "application/json")
-            .with_body(json_string)
-            .create();
-
-        let repository = PostRepository::new(server.url());
-        // let mock = make_response(json_string);
-
-        println!("sentinel3");
         assert_eq!(repository.find_posts(1, 1).await?, posts);
-        mock.assert();
-        mock.remove();
+        mock.1.assert();
+        mock.1.remove();
         Ok(())
     }
 
-    // fn server() -> ServerGuard {
-    //     mockito::Server::new()
-    // }
-    //
-    // fn make_response(json_string: String) -> Mock {
-    //     // Request a new server from the pool
-    //     let mut server = server();
-    //     println!("sentinel1");
-    //
-    //     // Use one of these addresses to configure your client
-    //     let host = server.host_with_port();
-    //     let url = server.url();
-    //
-    //     println!("{}", host);
-    //     println!("{}", url);
-    //     println!("{}", json_string);
-    //     println!("{}", server.host_with_port());
-    //     // Create a mock
-    //
-    //     server
-    //         .mock("GET", "/posts?per_page=1&offset=1")
-    //         .with_status(201)
-    //         .with_header("content-type", "application/json")
-    //         .with_body(json_string)
-    //         .create()
-    //
-    //     // You can use `Mock::assert` to verify that your mock was called
-    //     // mock.assert();
-    // }
+    fn create_mock_server(
+        path: String,
+        json_string: String,
+        status_code: Option<usize>,
+    ) -> (mockito::ServerGuard, mockito::Mock) {
+        let status_code = status_code.unwrap_or(200);
+        let mut server = mockito::Server::new();
+        let mock = server
+            .mock("GET", path.as_str())
+            .with_status(status_code)
+            .with_header("content-type", "application/json")
+            .with_body(json_string)
+            .create();
+        (server, mock)
+    }
 }
